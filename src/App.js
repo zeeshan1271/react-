0882c5457e718085353a340e2795005170344375
Aasim <aasimraza.ddevops@gmail.com>
2023-02-03 14:11:32 +0500

    react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header App-link">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+
+          Welcome to DDevops Academy
+      </header>
+    </div>
+  );
+}
+
+export default App;
